e601e3706d71242b111645ed8a7f8baf689098c0
Tamila Kochereshchenko <tamila272005@icloud.com>
2025-10-30 18:15:15 +0200

    fetch people from the API when PeoplePage is opened

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,136 +1,82 @@
+import React, { useEffect, useState } from 'react';
 import { Loader } from './Loader';
+import { Person } from '../types';
+import { getPeople } from '../api';
 
-export const PeoplePage = () => (
-  <>
-    <h1 className="title">People Page</h1>
-    <div className="block">
-      <div className="box table-container">
-        <Loader />
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const loadPeople = async () => {
+    setLoading(true);
+    setError(null);
 
-        <p data-cy="peopleLoadingError" className="has-text-danger">
-          Something went wrong
-        </p>
+    try {
+      const data = await getPeople();
 
-        <p data-cy="noPeopleMessage">There are no people on the server</p>
+      setPeople(data);
+    } catch (e) {
+      setError('Something went wrong');
+    } finally {
+      setLoading(false);
+    }
+  };
 
-        <table
-          data-cy="peopleTable"
-          className="table is-striped is-hoverable is-narrow is-fullwidth"
-        >
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Sex</th>
-              <th>Born</th>
-              <th>Died</th>
-              <th>Mother</th>
-              <th>Father</th>
-            </tr>
-          </thead>
+  useEffect(() => {
+    loadPeople();
+  }, []);
 
-          <tbody>
-            <tr data-cy="person">
-              <td>
-                <a href="#/people/jan-van-brussel-1714">Jan van Brussel</a>
-              </td>
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+      <div className="block">
+        <div className="box table-container">
+          {loading && <Loader />}
 
-              <td>m</td>
-              <td>1714</td>
-              <td>1748</td>
-              <td>Joanna van Rooten</td>
-              <td>Jacobus van Brussel</td>
-            </tr>
+          {!loading && error && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              {error}
+            </p>
+          )}
 
-            <tr data-cy="person">
-              <td>
-                <a href="#/people/philibert-haverbeke-1907">
-                  Philibert Haverbeke
-                </a>
-              </td>
+          {!loading && !error && people.length === 0 && (
+            <p data-cy="noPeopleMessage">There are no people on the server</p>
+          )}
 
-              <td>m</td>
-              <td>1907</td>
-              <td>1997</td>
+          {!loading && !error && people.length > 0 && (
+            <table
+              data-cy="peopleTable"
+              className="table is-striped is-hoverable is-narrow is-fullwidth"
+            >
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Sex</th>
+                  <th>Born</th>
+                  <th>Died</th>
+                  <th>Mother</th>
+                  <th>Father</th>
+                </tr>
+              </thead>
 
-              <td>
-                <a
-                  className="has-text-danger"
-                  href="#/people/emma-de-milliano-1876"
-                >
-                  Emma de Milliano
-                </a>
-              </td>
-
-              <td>
-                <a href="#/people/emile-haverbeke-1877">Emile Haverbeke</a>
-              </td>
-            </tr>
-
-            <tr data-cy="person" className="has-background-warning">
-              <td>
-                <a href="#/people/jan-frans-van-brussel-1761">
-                  Jan Frans van Brussel
-                </a>
-              </td>
-
-              <td>m</td>
-              <td>1761</td>
-              <td>1833</td>
-              <td>-</td>
-
-              <td>
-                <a href="#/people/jacobus-bernardus-van-brussel-1736">
-                  Jacobus Bernardus van Brussel
-                </a>
-              </td>
-            </tr>
-
-            <tr data-cy="person">
-              <td>
-                <a
-                  className="has-text-danger"
-                  href="#/people/lievijne-jans-1542"
-                >
-                  Lievijne Jans
-                </a>
-              </td>
-
-              <td>f</td>
-              <td>1542</td>
-              <td>1582</td>
-              <td>-</td>
-              <td>-</td>
-            </tr>
-
-            <tr data-cy="person">
-              <td>
-                <a href="#/people/bernardus-de-causmaecker-1721">
-                  Bernardus de Causmaecker
-                </a>
-              </td>
-
-              <td>m</td>
-              <td>1721</td>
-              <td>1789</td>
-
-              <td>
-                <a
-                  className="has-text-danger"
-                  href="#/people/livina-haverbeke-1692"
-                >
-                  Livina Haverbeke
-                </a>
-              </td>
-
-              <td>
-                <a href="#/people/lieven-de-causmaecker-1696">
-                  Lieven de Causmaecker
-                </a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+              <tbody>
+                {people.map(person => (
+                  <tr data-cy="person" key={person.slug}>
+                    <td>
+                      <a href={`#/people/${person.slug}`}></a>
+                    </td>
+                    <td>{person.sex}</td>
+                    <td>{person.born}</td>
+                    <td>{person.died}</td>
+                    <td>{person.motherName || '-'}</td>
+                    <td>{person.fatherName || '-'}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
