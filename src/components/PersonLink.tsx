1803da9d849007c4cac581cb000ed4ce0dec7578
Tamila Kochereshchenko <tamila272005@icloud.com>
2025-10-31 22:04:58 +0200

    chande <a> to <Link> in PersonLink

--- src/components/PersonLink.tsx
+++ src/components/PersonLink.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Person } from '../types';
+import { Link } from 'react-router-dom';
 
 interface Props {
   name: string | null;
@@ -18,11 +19,11 @@ export const PersonLink: React.FC<Props> = ({ name, allPeople }) => {
   }
 
   return (
-    <a
-      href={`#/people/${person.slug}`}
+    <Link
+      to={`/people/${person.slug}`}
       className={person.sex === 'f' ? 'has-text-danger' : 'has-text-link'}
     >
       {person.name}
-    </a>
+    </Link>
   );
 };
