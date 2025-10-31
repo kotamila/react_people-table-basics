f3eba840c535710a08323a7b7f60ca9498d3a877
Tamila Kochereshchenko <tamila272005@icloud.com>
2025-10-31 20:52:34 +0200

    fix select a person

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ export const App = () => (
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/people" element={<PeoplePage />} />
+          <Route path="/people/:slug" element={<PeoplePage />} />
           <Route path="/home" element={<Navigate to="/" replace />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
